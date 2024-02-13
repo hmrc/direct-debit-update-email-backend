4aa2411173564cfa3c6a5a9f13913926181070ef
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2024-02-13 15:46:55 +0000

    [AC][OPS-11769] library updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,31 +4,31 @@ object AppDependencies {
 
   private val playVersion = "-play-30"
 
-  private val bootstrapVersion = "8.2.0"
-  private val hmrcMongoVersion = "1.6.0"
+  private val bootstrapVersion = "8.4.0"
+  private val hmrcMongoVersion = "1.7.0"
   private val enumeratumVersion = "1.7.3"
   private val enumeratumPlayVersion = "1.8.0"
   private val catsVersion = "2.10.0"
   private val cryptoVersion = "7.6.0"
   private val playJsonDerivedCodesVersion = "10.1.0"
-  private val chimneyVersion = "0.8.3"
+  private val chimneyVersion = "0.8.5"
 
   lazy val microserviceDependencies: Seq[ModuleID] = {
 
     val compile = Seq(
       // format: OFF
-      "uk.gov.hmrc"          %% s"internal-auth-client$playVersion" % "1.9.0",
+      "uk.gov.hmrc"          %% s"internal-auth-client$playVersion" % "1.10.0",
       "uk.gov.hmrc"          %% s"bootstrap-backend$playVersion"    % bootstrapVersion,
       "com.beachape"         %% "enumeratum"                        % enumeratumVersion,
       "io.scalaland"         %% "chimney"                           % chimneyVersion
-     // format: ON
+    // format: ON
     )
 
     val test = Seq(
       // format: OFF
       "uk.gov.hmrc"       %% s"bootstrap-test$playVersion"  % bootstrapVersion,
       "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test$playVersion" % hmrcMongoVersion
-      // format: ON
+    // format: ON
     ).map(_ % Test)
 
     compile ++ test
@@ -42,6 +42,6 @@ object AppDependencies {
     "com.beachape"      %% "enumeratum-play"               % AppDependencies.enumeratumPlayVersion,
     "org.julienrf"      %% "play-json-derived-codecs"      % AppDependencies.playJsonDerivedCodesVersion,
     "org.typelevel"     %% "cats-core"                     % catsVersion
-    // format: ON
+  // format: ON
   )
 }
